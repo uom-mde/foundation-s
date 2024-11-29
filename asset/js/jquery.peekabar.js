4b723a411d2d3bd8c6092698578ae630863c4b78
johndmccrory <100780899+johndmccrory@users.noreply.github.com>
2024-11-29 12:29:31 +0000

    Revised content warning banner with 1) a view partial, and 2. conditions depending on the site

--- asset/js/jquery.peekabar.js
+++ asset/js/jquery.peekabar.js
@@ -16,7 +16,7 @@
     this.bar = {};
     this.settings = {};
     var defaults = {
-      html: 'Content Warning: This site contains material you may find inappropriate. <a href="#">View our policy on sensitive material</a>.',
+      html: 'Content Warning: This site contains material you may find upsetting. <a href="#">View our policy on sensitive material</a>.',
       delay: 30000,
       autohide: true,
       padding: '1em',
